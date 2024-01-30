ddc5246a73f19294af47a230beac5bab9342c686
Ideasg <prabhugideasg89@gmail.com>
2024-01-30 13:11:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+(function () {
+    setTimeout(function() {
+        document.getElementById("container").innerHTML = "10";
+      }, 1000);
+      setTimeout(function() {
+        document.getElementById("container").innerHTML = "9";
+      }, 2000);
+      setTimeout(function() {
+        document.getElementById("container").innerHTML = "8";
+      }, 3000);      
+      setTimeout(function() {
+        document.getElementById("container").innerHTML = "7";
+      }, 4000);      
+      setTimeout(function() {
+        document.getElementById("container").innerHTML = "6";
+      }, 5000);      
+      setTimeout(function() {
+        document.getElementById("container").innerHTML = "5";
+      }, 6000);      
+      setTimeout(function() {
+        document.getElementById("container").innerHTML = "4";
+      }, 7000);      
+      setTimeout(function() {
+        document.getElementById("container").innerHTML = "3";
+      }, 8000);      
+      setTimeout(function() {
+        document.getElementById("container").innerHTML = "2";
+      }, 9000);      
+      setTimeout(function() {
+        document.getElementById("container").innerHTML = "1";
+      }, 10000);
+      setTimeout(function() {
+        document.getElementById("container").innerHTML = "<img src='Happy Independence Day.jpg'>";
+      }, 11000);
+})();
